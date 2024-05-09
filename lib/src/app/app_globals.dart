bbe6b939fbd0e07f8749204b64b40f93c4524f2a
phatdat.dev <phatdatfbi@gmail.com>
2024-05-09 16:03:03 +0700

    [Fix] platform web

--- lib/src/app/app_globals.dart
+++ lib/src/app/app_globals.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 abstract class AppGlobals {
-  static final kTestMode = Platform.environment.containsKey('FLUTTER_TEST');
+  static final kTestMode = !kIsWeb ? Platform.environment.containsKey('FLUTTER_TEST') : false;
   static final rootNavigatorKey = GlobalKey<NavigatorState>();
   static BuildContext get context => rootNavigatorKey.currentContext!;
   static String lastCallUrlApi = "";
