40e142b39bf11af059f2f9abfa4dcb20103caf64
phatdat.dev <phatdatfbi@gmail.com>
2024-08-30 10:41:56 +0700

    [Feat] update extension getMaterialColor

--- lib/src/extensions/color_extensions.dart
+++ lib/src/extensions/color_extensions.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../app/color_constants.dart';
+
 extension ColorExtension on Color {
   ColorFilter toColorFilter() => ColorFilter.mode(this, BlendMode.srcIn);
+
+  MaterialColor toMaterialColor() => ColorConstants.getMaterialColor(this);
 }
